0bf2647cf52035a3d3e9bb198dc0694b266f0aff
Jony <safiulalomjony@gmail.com>
2023-12-02 16:02:26 +0600

    added puppeteer extra

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
 const express = require('express');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-    res.send('Email: ' + req.query.email);
+    const email = req.query.email ? req.query.email : 'example@gmail.com';
+    
+    (async () => {
+      const browser = await puppeteer.launch({ headless: false})
+      const page = await browser.newPage()
+    
+      const navigationPromise = page.waitForNavigation()
+    
+      await page.goto('https://accounts.google.com/')
+    
+      await navigationPromise
+    
+      await page.waitForSelector('input[type="email"]')
+      await page.click('input[type="email"]')
+    
+      await navigationPromise
+    
+      //TODO : change to your email 
+      await page.type('input[type="email"]', email)
+    
+      await page.waitForSelector('#identifierNext')
+      await page.click('#identifierNext')
+    
+      await page.waitForTimeout(500);
+    
+      await page.waitForSelector('input[type="password"]')
+      await page.click('input[type="email"]')
+      await page.waitForTimeout(500);
+    
+      await navigationPromise
+    
+      await page.waitForTimeout(5000);
+    
+      console.log(page.url());    
+      
+      res.send('Response: ' + page.url());
+
+      await browser.close();
+    })()
 })
 
 app.listen(PORT, () => {
